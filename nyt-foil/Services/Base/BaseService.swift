75a74c59fa08faea56ef6512c16f33ebf827f322
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-17 22:03:40 -0300

    :sparkles: Creates BaseService

---
+++ nyt-foil/Services/Base/BaseService.swift
@@ -0,0 +1,35 @@
+//
+//  BaseService.swift
+//  nyt-foil
+//
+//  Created by Luiz Felipe Aires Soares on 17/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol BaseService: BaseServiceProtocol {
+    
+}
+
+extension BaseService {
+    
+    func request<T: Decodable>(provider: MoyaProvider<NYTAPI> = MoyaProvider<NYTAPI>(plugins: [NetworkLoggerPlugin(verbose: true)]), _ target: NYTAPI, completion: @escaping (Result<T, NYTAPIError>) -> Void) {
+        provider.request(target) { (result) in
+            switch result {
+            case .success(let response):
+                do {
+                    let object: T = try JSONDecoder().decode(T.self, from: response.data)
+                    completion(.success(object))
+                } catch {
+                    completion(.failure(.jsonMapping))
+                }
+            case .failure(let error):
+                print(error)
+                completion(.failure(.unauthorized))
+            }
+        }
+    }
+    
+}
