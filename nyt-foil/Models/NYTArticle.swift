340a84402a356fa222e4dc2bf05e951138a3a8cc
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-17 22:05:14 -0300

    :sparkles: Creates NYTArticle entity

---
+++ nyt-foil/Models/NYTArticle.swift
@@ -0,0 +1,44 @@
+//
+//  NYTArticle.swift
+//  nyt-foil
+//
+//  Created by Luiz Felipe Aires Soares on 17/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import Foundation
+
+struct NYTArticle: Decodable {
+    
+    let uri: String
+    let url: String
+    let id: Int
+    let source: String
+    let publishedDate: String
+    let updated: String
+    let section: String
+    let subsection: String
+    let nytdsection: String
+    let byline: String
+    let type: String
+    let title: String
+    let abstract: String
+    let media: [NYTMedia]
+    
+    private enum CodingKeys: String, CodingKey {
+        case uri
+        case url
+        case id
+        case source
+        case publishedDate  = "published_date"
+        case updated
+        case section
+        case subsection
+        case nytdsection
+        case byline
+        case type
+        case title
+        case abstract
+        case media
+    }
+}
