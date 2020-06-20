50b93f7faa34f61818189617c600697839626c80
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-20 12:02:23 -0300

    :sparkles: Creates CoordinatorProtocol

---
+++ nyt-foil/Utils/Coordinators/CoordinatorProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CoordinatorProtocol.swift
+//  nyt-foil
+//
+//  Created by Luiz Felipe Aires Soares on 20/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import UIKit
+
+protocol CoordinatorProtocol {
+    
+    func make() -> UIViewController
+    
+}
