1613340597e26c7d3a2685f2fda083d72ee2dc5e
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-20 12:03:48 -0300

    :sparkles: Creates ListView

---
+++ nyt-foil/Modules/ListModule/View/ListView.swift
@@ -0,0 +1,53 @@
+//
+//  ListView.swift
+//  nyt-foil
+//
+//  Created by Luiz Felipe Aires Soares on 20/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import UIKit
+
+class ListView: UIView {
+    
+    // MARK: - Private Properties
+    
+    private enum Constants {
+        static let edge: CGFloat = 8.0
+    }
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.estimatedRowHeight = 208
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: "ListTableViewCell")
+        tableView.tableFooterView = UIView(frame: .zero)
+        return tableView
+    }()
+    
+    // MARK: - Init
+    
+    required init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        setupSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Functions
+    
+    private func setupSubviews() {
+        addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(Constants.edge * 2)),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: (Constants.edge * 2)),
+        ])
+    }
+    
+}
