d6e2504d5dfde9ddf4abfe5820e5b0b381539060
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-20 17:30:21 -0300

    :art: Creates delegate protocol; Adds segmented control for period

--- nyt-foil/Modules/ListModule/View/ListView.swift
+++ nyt-foil/Modules/ListModule/View/ListView.swift
@@ -8,25 +8,47 @@
 
 import UIKit
 
+protocol ListViewDelegate: AnyObject {
+    
+    func didSelectPeriod(_ period: Int)
+    
+}
+
 class ListView: UIView {
     
     // MARK: - Private Properties
     
     private enum Constants {
         static let edge: CGFloat = 8.0
+        
+        enum PeriodControl {
+            static let height: CGFloat = 40.0
+        }
     }
+    
+    lazy var periodControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["1 day", "7 days", "30 days"])
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.tintColor = .black
+        segmentedControl.addTarget(self, action: #selector(periodChanged(_:)), for: .valueChanged)
+        segmentedControl.selectedSegmentIndex = 1
+        return segmentedControl
+    }()
     
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.backgroundColor = .white
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
         tableView.estimatedRowHeight = 208
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorStyle = .none
-        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: "ListTableViewCell")
         tableView.tableFooterView = UIView(frame: .zero)
+        tableView.contentInset = UIEdgeInsets(top: -32.0, left: 0.0, bottom: 0.0, right: 0.0)
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: "ListTableViewCell")
         return tableView
     }()
+    
+    weak var delegate: ListViewDelegate?
     
     // MARK: - Init
     
@@ -43,13 +65,43 @@ class ListView: UIView {
     // MARK: - Private Functions
     
     private func setupSubviews() {
+        addSubview(periodControl)
+        setupPeriodControlConstraints()
         addSubview(tableView)
+        setupTableViewConstraints()
+    }
+    
+    private func setupPeriodControlConstraints() {
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: topAnchor),
+            periodControl.topAnchor.constraint(equalTo: topAnchor, constant: Constants.edge),
+            periodControl.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(Constants.edge * 2)),
+            periodControl.leadingAnchor.constraint(equalTo: leadingAnchor, constant: (Constants.edge * 2)),
+            periodControl.heightAnchor.constraint(equalToConstant: Constants.PeriodControl.height)
+        ])
+    }
+    
+    private func setupTableViewConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: periodControl.bottomAnchor, constant: Constants.edge),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(Constants.edge * 2)),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: (Constants.edge * 2)),
         ])
+    }
+    
+    @IBAction private func periodChanged(_ sender: UISegmentedControl) {
+        var period = 1
+        switch sender.selectedSegmentIndex {
+        case 0:
+            period = 1
+        case 1:
+            period = 7
+        case 2:
+            period = 30
+        default:
+            return
+        }
+        delegate?.didSelectPeriod(period)
     }
     
 }
