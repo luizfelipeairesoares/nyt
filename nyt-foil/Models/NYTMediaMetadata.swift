4fd6d10ae1d564a389cb7e480f95932e9cd044a6
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-20 16:45:09 -0300

    :art: Adds Equatable protocol and computed variable

--- nyt-foil/Models/NYTMediaMetadata.swift
+++ nyt-foil/Models/NYTMediaMetadata.swift
@@ -8,11 +8,45 @@
 
 import Foundation
 
+enum NYTMediaMetadataSize: Equatable {
+    
+    case standard
+    case double
+    case triple
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case (.standard, .standard):
+            return true
+        case (.double, .double):
+            return true
+        case (.triple, .triple):
+            return true
+        default:
+            return false
+        }
+    }
+    
+}
+
 struct NYTMediaMetadata: Decodable {
     
     let url: String
     let format: String
     let height: Int
     let width: Int
+    
+    var size: NYTMediaMetadataSize {
+        switch format {
+        case "Standard Thumbnail":
+            return .standard
+        case "mediumThreeByTwo210":
+            return .double
+        case "mediumThreeByTwo440":
+            return .triple
+        default:
+            return .standard
+        }
+    }
     
 }
