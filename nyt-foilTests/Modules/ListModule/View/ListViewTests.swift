ce37437ff038253b843e00863933ae67c9bd36e9
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-20 21:34:22 -0300

    :sparkles: Creates ListView test class

---
+++ nyt-foilTests/Modules/ListModule/View/ListViewTests.swift
@@ -0,0 +1,41 @@
+//
+//  ListViewTests.swift
+//  nyt-foilTests
+//
+//  Created by Luiz Felipe Aires Soares on 20/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import XCTest
+@testable import NYT
+
+class SpyDelegate: ListViewDelegate {
+    
+    var capturedAction: Bool = false
+    
+    func didSelectPeriod(_ period: Int) {
+        capturedAction = true
+    }
+    
+}
+
+class ListViewTests: XCTestCase {
+    
+    var listView: ListView!
+
+    override func setUpWithError() throws {
+        listView = ListView()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testDelegate() throws {
+        let delegate = SpyDelegate()
+        listView.delegate = delegate
+        listView.periodChanged(listView.periodControl)
+        XCTAssertTrue(delegate.capturedAction)
+    }
+
+}
